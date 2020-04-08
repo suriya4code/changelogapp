92b1df64d52ab264fe4487ae5a1e11de395ddafa
suriya4code <41639563+suriya4code@users.noreply.github.com>
2020-04-08 20:25:34 +0530

    Update dashboard.service.ts

--- src/app/dashboard.service.ts
+++ src/app/dashboard.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class DashboardService {
-  baseurl = 'http://localhost:3000/api/v1/change';
+  baseurl = 'https://changelogbackendapi.herokuapp.com/api/v1/change';
 
   constructor(private httpclient: HttpClient) {}
 
