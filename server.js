b10b61dd87d4ac306bc3f41244c94b3367bdafca
suriya4code <41639563+suriya4code@users.noreply.github.com>
2020-04-08 19:46:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist/{{your-app-name}}'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/{{your-app-name}}/index.html'));
+res.sendFile(path.join(__dirname,'/dist/changelogapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
